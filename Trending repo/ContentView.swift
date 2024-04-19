6c922cdab3af9522283231709b3f651df1e2325a
miadjalili <jalilimiad@gmail.com>
2024-04-19 10:57:58 -0700

    Initial Commit

---
+++ Trending repo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Trending repo
+//
+//  Created by Miad Jalili on 2024-04-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Trending repo/Trending_repoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trending_repoApp.swift
+//  Trending repo
+//
+//  Created by Miad Jalili on 2024-04-19.
+//
+
+import SwiftUI
+
+@main
+struct Trending_repoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
